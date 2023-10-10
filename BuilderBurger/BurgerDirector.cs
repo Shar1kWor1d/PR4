b81ae1834c0e24f2eca6c4edbd8ab70412216bd1
323 <323@323-12>
2023-10-10 10:34:28 +0300

    first commit

---
+++ BuilderBurger/BurgerDirector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PR4.DBCon;
+
+namespace PR4.BuilderBurger
+{
+    public class BurgerDirector
+    {
+        private readonly IBurgerBuilder _builder;
+        public BurgerDirector(IBurgerBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        public void BuildDefault()
+        {
+            _builder
+            .AddCheese()
+            .AddTomato()
+            .AddPickles();
+        }
+
+        public void BuildwithBeacon()
+        {
+            _builder
+                .AddTomato()
+                .AddBacon()
+                .AddLetuce();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PR4.BuilderBurger;
+using PR4.DBCon;
+
+namespace PR4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ModelEF model = new ModelEF();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            BurgerBuilder burgerBuilder = new BurgerBuilder();
+            BurgerDirector burgerDirector = new BurgerDirector(burgerBuilder);
+
+            if (ComboBoxBurger.SelectedIndex== 0)
+             burgerDirector.BuildDefault(); 
+            else
+             burgerDirector.BuildwithBeacon(); 
+
+            try
+            {
+                model.Burgers.Add(burgerBuilder.GetBurgers());
+                model.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            ComboBoxBurger.SelectedIndex = 0;
+            dataGridView1.DataSource = model.Burgers.ToList();
+        }
+    }
+}
